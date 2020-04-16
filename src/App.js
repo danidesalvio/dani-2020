5468710be659dbd05d503961d39f8678c1dc325c
Dani DeSalvio <danidvisuals@gmail.com>
2020-04-16 17:51:12 -0600

    update app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Route, Switch } from 'react-router-dom';
+import Nav from './components/shared/Nav';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Work from './components/pages/Work';
+import Footer from './components/shared/Footer';
+import Blog from './components/pages/blog/Blog';
 
 const App = () => (
   <>
+    <Nav />
       <Switch>
-        <Route />
-        <Route />
-        <Route />
+        <Route exact path="/" component={Home} />
+        <Route eaxct path="/about" component={About} />
+        <Route exact path="/work" component={Work}/>
+        <Route exact path="/blog" component={Blog}/>
       </Switch>
+
+      <Footer />
   </>
 )
 
