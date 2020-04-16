7a4eae600a147d98e82e13ee94f3ae8da02a7f36
Dani DeSalvio <danidvisuals@gmail.com>
2020-04-16 17:25:56 -0600

    add pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Vimeo from '@u-wave/react-vimeo';
+
+class Home extends Component {
+    state = {}
+
+  handleShow = () => this.setState({ active: true })
+  handleHide = () => this.setState({ active: false })
+
+  render() {
+    const { active } = this.state
+    const content = (
+      <div>
+        <Header as='h2' inverted>
+          
+        </Header>
+        <Button primary>View</Button>
+      </div>
+    )
+
+    return (
+    <>
+    <div class="video-header">
+      <div class="video-content">
+          <h1>Dani D Visuals</h1>
+          <h3>creating visual experiences for all</h3>
+      </div>
+      <div class="video-overlay"></div>
+      <div class="video-background">
+          <Vimeo
+            video="315734121"
+            background='true'
+            showTitle='false'
+            showPortrait='false'
+            showByline='false'
+            controls='false'
+            className="video-bg"
+            autoplay='true'
+            responsive='true'
+            loop='true'
+            />
+      </div>
+    </div>
+    <Work/>
+    </>
+    )
+  }
+}
+
+export default Home;
---
+++ src/components/shared/Nav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+const Nav = () => (
+    <div class="nav">
+      <div class="nav-logo">
+        <a href="/"><img src={logo}/></a>
+      </div>
+        <div class="nav-items">
+            <a href='/'>Home</a>
+            <a href='/about'>About</a>
+            <a href='/work'>Work</a>
+            <a href='/blog'>Blog</a>
+        </div>
+        
+        {/* Slide in Menu */}
+        {/* <div className="menu" onclick={MenuIn}>
+            <div className='nav-text'>MENU</div>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+        </div> */}
+    </div>
+)
+
+export default Nav;
