04981b2cdb1ec5c5c9ce97b5184c3382fd73d5fe
Dani DeSalvio <danidvisuals@gmail.com>
2020-04-16 18:17:33 -0600

    import css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Nav from './components/shared/Nav';
 import Home from './components/pages/Home';
